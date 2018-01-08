35ec308054b5a9c51c382523863791879cee0f86
Kayla Webb <kaylarwebb@gmail.com>
2018-01-08 17:22:32 -0500

    Implement event listener for the hamburger

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 'use strict';
+/*global $ */
+
+//ES5
+// $(document).ready(function() {
+//   console.log('Ready!');
+// });
+
+//ES6
+
+const main = () => {
+  $('.hamburger').on('click', () => {
+    console.log('I was clicked!');
+  });
+};
+
+$(() => {
+  main();
+  console.log('ready!');
+});
